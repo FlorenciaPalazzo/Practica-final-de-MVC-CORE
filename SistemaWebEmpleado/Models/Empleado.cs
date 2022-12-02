4893e4b5b0d02803f1057e1de1afefbf314e9a0c
FlorenciaPalazzo <florencia.e.palazzo@gmail.com>
2022-12-02 18:18:40 -0300

    segunda parte- Finalizada

--- SistemaWebEmpleado/Models/Empleado.cs
+++ SistemaWebEmpleado/Models/Empleado.cs
@@ -14,8 +14,8 @@ namespace SistemaWebEmpleado.Models
         [Required]
         public string DNI { get; set; }
 
-        [Required]
         [CheckLegajo]
+        [Required]
         public string Legajo { get; set; }
         [Required]
         public string Titulo { get; set; }
